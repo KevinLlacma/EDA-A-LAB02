5d1efa199a2cbb0f98883f4bd5c586dce029944d
klismannSis <achaisa@unsa.edu.pe>
2023-05-19 08:12:34 -0500

    primer avance

---
+++ Ejercicio4/Lista.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Lista {
+    ArrayList<String> listaA;
+    public Lista(String a){
+        listaA = new ArrayList<String>();
+    }
+    public boolean add(String element){
+        return listaA.add(element);
+    }
+    
+}
